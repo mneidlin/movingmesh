38790ee9d8ae3e31a6e4d71b85adf5f5dbedb850
nikithiel <niklas.thiel@rwth-aachen.de>
2022-01-10 15:22:48 +0100

    Further cleaning and rearranging of code.

--- udf_exeDynMesh.c
+++ udf_exeDynMesh.c
@@ -1,125 +1,128 @@
-﻿/* This UDF reads the "UDFPTS" folder that has to be in the FLUENT simulation location and prescribes the movement of the nodes*/
+﻿/* This UDF reads the "UDFPTS" folder that has to be in the FLUENT simulation location and prescribes the movement of the nodes */
 #include "udf.h"
+#include "dynamesh_tools.h"
 
-#define nTimeSteps 241 /* output of ps_intNPts  "Total Number of Frames after interpolation" */
-#define allPtsNum 10353 /* output of ps_detNPts*/
+#define nTimeSteps 1236	/* total number of frames after interpolation: output of ps_intNPts */
+#define allPtsNum 3260	/* total number of nodes on moving boundaries: output of ps_detNPts */
 
-#define Scale 0.001 /*factor was considered in udf_extPts (AssignID)*/  
+#define Scale 0.001 	/* factor for scaling units; already considered in udf_extPts (AssignID) */  
 #define fakeTime 0
 
-/*Define the number of nodes per core (except for the last one) 
-Values are provided by AssignID (udf_extPts) in the console, adapt from there */  
-const static int PtsNum[] = { 1319, 1334, 1425, 1224, 1066, 987, 970, 699, 669, 660 } ;
+/* define the number of nodes per core (except for the last one); values are provided by AssignID (udf_extPts) in the console, adapt from there */  
+const static int PtsNum[] = { 1645, 1615 };
 
 static real vel, Grid_X[allPtsNum], Grid_Y[allPtsNum], Grid_Z[allPtsNum];
 
-
-/*Assigns loaded node coordinates to the 3 zones (ventricle, inlet, outlet)*/
+/* assigns loaded node coordinates to the face zones (ventricle, inlet, outlet); note: function gets called by each dynamic mesh zone 
+that uses this UDF for mesh motion. Further, smoothing applies a pre-processing step and thus function gets called twice 
+(only the single mesh motion gets applied though) */
 DEFINE_GRID_MOTION(Ven_move, domain, dt, time, dtime)
 {
-	#if !RP_HOST /* if you don’t exclude the host, this operation will result in a division by zero and error!
-	Remember that host has no data so its total will be zero. */
-	Thread *tf=DT_THREAD(dt);
-	face_t f;
-	Node *node_p;
-	#endif
-
-	real X[3];
-	int nTime,n,ptID, i;
-	
-	nTime=(N_TIME+fakeTime)%nTimeSteps+1; /* N_TIME integer number of time steps; nTime: actual timestep in cycle
-												-> restarts at 1 after one cycle */
-
 	#if !RP_HOST
-	if (!Data_Valid_P()) /* Do nothing if gradient isn’t allocated yet. */
-		return;
+		Thread *tf=DT_THREAD(dt);
+		face_t f;
+		Node *node_p;
+
+		real X[3];
+		int nTime, n, ptID, i;
+
+		nTime=(N_TIME+fakeTime)%nTimeSteps+1; /* N_TIME integer number of time steps; nTime: actual timestep in cycle; restarts at 1 after one cycle */
+
+		if (!Data_Valid_P()) /* do nothing if gradient isn’t allocated yet. */
+			return;
 	
-	SET_DEFORMING_THREAD_FLAG(THREAD_T0(tf));
-	begin_f_loop(f,tf)
-	if PRINCIPAL_FACE_P(f,tf) /* tests if the face is the principle face 
-								 although each face can appear on one or two partitions, it can only "officially'' belong to one of them.*/
-	{
-		f_node_loop(f,tf,n)
+		SET_DEFORMING_THREAD_FLAG(THREAD_T0(tf));
+	
+		begin_f_loop(f,tf)
+		if PRINCIPAL_FACE_P(f,tf) /* checks if face truly belongs to computing node: important for faces at the partition interface */
 		{
-			node_p=F_NODE(f,tf,n);  /* obtain the global face node number*/
-			if (NODE_POS_NEED_UPDATE(node_p))
+			f_node_loop(f,tf,n)
 			{
-			  NODE_POS_UPDATED(node_p);
+				node_p=F_NODE(f,tf,n);  /* obtain the global face node number*/
+				if (NODE_POS_NEED_UPDATE(node_p))
+				{
+			  		NODE_POS_UPDATED(node_p);
 
-			  ptID=N_UDMI(node_p,myid);
+			  		ptID=N_UDMI(node_p,myid);
 
-			  if (myid != 0)
-			  {
-			  	for (i = 0 ; i < myid ; i++)
-			  	{
-			  		ptID = ptID + PtsNum[i];
-			  	}
-			  }
+					/* obtain global index in array from offset due to other computing nodes; array sequentially ordered from node_0 to node_(N-1) */
+			  		if (myid != 0)
+			  		{
+			  			for (i = 0 ; i < myid ; i++)
+			  			{	
+			  				ptID = ptID + PtsNum[i];
+			  			}
+			  		}	
 
-			  X[0]=Grid_X[ptID];
-			  X[1]=Grid_Y[ptID];
-			  X[2]=Grid_Z[ptID];
-			  /*Message("\n node: %d, X: %lg, Y:%lg, Z: %lg", ptID, X[0], X[1], X[2]);*/
+			  		X[0]=Grid_X[ptID];
+			  		X[1]=Grid_Y[ptID];
+			  		X[2]=Grid_Z[ptID];
 
-			  NODE_X(node_p)=X[0]*Scale;
-			  NODE_Y(node_p)=X[1]*Scale;
-			  NODE_Z(node_p)=X[2]*Scale;
-
+			  		NODE_X(node_p)=X[0]*Scale;
+			  		NODE_Y(node_p)=X[1]*Scale;
+			  		NODE_Z(node_p)=X[2]*Scale;
 			}
 		}
 	}
 	end_f_loop(f,tf)
-	#endif
+	#endif /* RP_NODE */ 
 }
 
-/*loads after successful timestep the new point coordinates from udfsurface file
-function set at Funktion Hooks bei Define at end (Fluent environment)*/
+/* at the end of timestep new node coordinates get loaded from udfsurface file; function hooked under "Define at end" */
 DEFINE_EXECUTE_AT_END(loadMESH)
 {
-	int nTime, i;
-	char nFileName[100];
-	FILE *meshFrame;
-	
-	nTime=(N_TIME+fakeTime)%nTimeSteps+1;
 	#if !RP_NODE
-	sprintf(nFileName,"UDFPTS/udfsurface_%d.asc",nTime); /* string saved in nFileName  */
-	meshFrame=fopen(nFileName,"r");
-	Message("\n %s \n", nFileName);
-	Message("\n %d \n", N_TIME);
-	for (i=0; i<allPtsNum; i++)
-	{
-		fscanf(meshFrame,"%lg %lg %lg", &Grid_X[i],&Grid_Y[i],&Grid_Z[i]); 
-	}
-	fclose(meshFrame);
-	Message("Finish Loading \n");
-	#endif
+		int nTime, i;
+		char nFileName[100];
+		FILE *meshFrame;
+	
+		nTime=(N_TIME+fakeTime)%nTimeSteps+1;
+
+		sprintf(nFileName,"UDFPTS/udfsurface_%d.asc",nTime); /* string saved in nFileName  */
+		meshFrame=fopen(nFileName,"r");
+		Message("\n %s \n", nFileName);
+		Message("\n %d \n", N_TIME);
+
+		for (i=0; i<allPtsNum; i++)
+		{
+			fscanf(meshFrame,"%lf %lf %lf", &Grid_X[i],&Grid_Y[i],&Grid_Z[i]); 
+		}
+	
+		fclose(meshFrame);
+		Message("Finish Loading \n");
+	#endif /* RP_NODE */ 
+
 	host_to_node_real(Grid_X,allPtsNum);  /* integer and real variables passed from host to nodes */
 	host_to_node_real(Grid_Y,allPtsNum);
 	host_to_node_real(Grid_Z,allPtsNum);
 }
 
-/*Funktionhas to be run before starting simulation, performs same as loadMESH function only for first timestep. Loads current mesh. */
+/* function has to be run before starting simulation, performs same as loadMESH function only for first timestep. Loads current mesh. */
 DEFINE_ON_DEMAND(First_loadMESH)
 {
-	int nTime, i;
-	char nFileName[100];
-	FILE *meshFrame;
-	
-	nTime=(N_TIME+fakeTime)%nTimeSteps+1;   /* N_TIME integer number of time steps; nTime remaining timesteps */
+	/* initial surface data gets read by host process */
 	#if !RP_NODE
+		int nTime, i;
+		char nFileName[100];
+		FILE *meshFrame;
+	
+		nTime=(N_TIME+fakeTime)%nTimeSteps+1;   /* N_TIME integer number of time steps; nTime remaining timesteps */
 
-	sprintf(nFileName,"UDFPTS/udfsurface_%d.asc",nTime);
-	meshFrame=fopen(nFileName,"r");
-	Message("\n %s \n", nFileName);
-	Message("\n %d \n", N_TIME);
-	for (i=0; i<allPtsNum; i++)
-	{
-		fscanf(meshFrame,"%lg %lg %lg", &Grid_X[i],&Grid_Y[i],&Grid_Z[i]);
-		/*Message("\n Werte %d: %lg %lg %lg \n", i, Grid_X[i], Grid_Y[i], Grid_Z[i]);*/
-	}
-	fclose(meshFrame);
-	Message("Finish Loading \n");
-	#endif
+		sprintf(nFileName,"UDFPTS/udfsurface_%d.asc",nTime);
+		meshFrame=fopen(nFileName,"r");
+		Message("\n %s \n", nFileName);
+		Message("\n %d \n", N_TIME);
+
+		for (i=0; i<allPtsNum; i++)
+		{
+			fscanf(meshFrame,"%lf %lf %lf", &Grid_X[i], &Grid_Y[i], &Grid_Z[i]);
+		}
+	
+		fclose(meshFrame);
+		Message("Finish Loading \n");
+	#endif /* RP_HOST */ 
+	
+	/* send data to each compute node */
 	host_to_node_real(Grid_X,allPtsNum);
 	host_to_node_real(Grid_Y,allPtsNum);
 	host_to_node_real(Grid_Z,allPtsNum);
--- udf_extPts.c
+++ udf_extPts.c
@@ -1,104 +1,106 @@
 /* UDF to assign nodes to a user defined memory location. The number of user-defined memory locations
-has to be the same as the number of used cores => Otherwise ANSYS crashes. In the end a "surface" file with the x,y,z coordinates of 
-the nodes is written*/
+has to be the same as the number of used cores => Otherwise ANSYS crashes. In the end a "surface" file with the local node numbering and x,y,z coordinates of 
+the corresponding nodes is written */
 
 #include "udf.h"
 
-/* Define zones of venctricle, inlet and outlet*/
+/* define zoneIDs of ventricle, inlet and outlet */
+#define nFZones 3
 #define ventricle 5      
 #define inlet 6
 #define outlet 7
 
-/* Set first entry of zoneID to the ventricle and provide output */
+/* assigns zoneIDs to an array */
 void loadZoneID15(int *zoneID)
 {
 	zoneID[0]=ventricle;
-	/*Message("Ventricle: %d \n", ventricle);*/
 	zoneID[1]=inlet;
-	/*Message("Inlet: %d \n", inlet);  */
 	zoneID[2]=outlet;
-	/*Message("Outlet: %d \n", outlet); */   
+	
+	if (I_AM_NODE_ZERO_P)
+	{
+		Message("\n Ventricle: %d", ventricle);
+		Message("\n Inlet: %d", inlet);
+		Message("\n Outlet: %d", outlet);
+	}
 }
 
-DEFINE_ON_DEMAND(First_AssignID)  /* Perform the UDF on demand */
+DEFINE_ON_DEMAND(First_AssignID)  /* perform the UDF on demand */
 {
 	#if !RP_HOST 
-	Domain *domain;  /* Get the domain using ANSYS FLUENT utility */
-	Thread *tFace;
-	face_t f;                    /* integer data type that identifies a particular face within a face thread */
-	Node *v;
+		Domain *domain;			/* get the domain using ANSYS FLUENT utility */
+		Thread *tFace;
+		face_t f;               /* integer data type that identifies a particular face within a face thread */
+		Node *v;
 	
-	int zoneID[3],i,n, ptID=0, dummy;             /* erzeugen der Integer */
-	real nodeX,nodeY,nodeZ;
-	FILE *ptFile;
-	int ptNum[100];
-
-	#if RP_NODE
-  		if (! I_AM_NODE_ZERO_P) PRF_CRECV_INT(myid - 1, &dummy, 1, myid - 1); 
-  		/*integer dummy is (1 element in array)sent from myid-1; For `receive' messages, the argument from is the ID of the sending node. 
-  		buffer is the name of an array of the appropriate type that will be received. nelem is the number of elements in the array and tag is the ID of the receiving node. 
-  		The tag convention for receive messages is the `from' node (same as the first argument). */
-		ptFile=fopen("surface",(I_AM_NODE_ZERO_P ? "w" : "a"));      /* open a file named surfacel in updating write mode and assign it to ptfile */
-	
-	#endif
-
-	domain=Get_Domain(1);
-
-	loadZoneID15(zoneID);                   /* Perform the functions*/
-
-	/* for-loop for the 3 zones ventricle, inlet and outlet
-	puts the user defined memory location of every node to -1*/
-	for (i=0;i<3;i++)
-	{
-		tFace=Lookup_Thread(domain,zoneID[i]);         /* Thread pointer to Zone ID */
-		begin_f_loop(f,tFace)
-		{
-			f_node_loop(f,tFace,n)      /* Loop over all nodes in face Thread tFace */
-			{
-				v=F_NODE(f,tFace,n);   /* obtain the global face node number*/
-				N_UDMI(v,myid)=-1;          /* access or store the value of the user-defined memory in a mesh node with index of the current core as -1*/
-			}
-		}
-		end_f_loop(f,tFace)
-	}
-	
-	/* for loop for the 3 zones ventricle, inlet and outlet. Goes through all nodes, that have not been checked (all with entries -1)
-and connects the node on the core with the index of the point. In other words, point ID and coordinates of each node is assigned in the file "surface" 	*/	
-	
-	for (i=0;i<3;i++)
-	{
-		tFace=Lookup_Thread(domain,zoneID[i]);        /* Thread pointer to Zone ID mit aufsteigenden Einträgen */
+		int zoneID[nFZones];	/* array containing face zone IDs */
+		int i, n;				/* indices for loops */
+		int ptID=0;				/* local node numbering on each computing node */
+		int dummy;				/* DUMMY; STILL TO BE DEFINED */
 		
-		begin_f_loop(f,tFace)      /*for loop over all faces in a given face thread tFace*/
+		real nodeX,nodeY,nodeZ;	/* local node coordinates */
+		FILE *ptFile;			/* pointer to file containing local node numbering and corresponding coordinates */ 
+		int ptNum[compute_node_count];
+		
+		/* dummy variable is received in round-robin fashion; each node receives from neighboring node to the left starting with node_0; other nodes are idle and 
+		wait for the respective send (see below); ensures sequential writing to file from 0 to N-1 */
+  		if (!I_AM_NODE_ZERO_P) PRF_CRECV_INT(myid - 1, &dummy, 1, myid - 1); 
+		
+		/* node_0 opens file in write mode and all other nodes in append mode */
+		ptFile=fopen("surface",(I_AM_NODE_ZERO_P ? "w" : "a"));
+
+		domain=Get_Domain(1);
+
+		/* loads zoneIDs of current simulation setup */
+		loadZoneID15(zoneID);
+
+		/* loop over all face zones and sets the user defined memory location of every computing node to -1 */
+		for (i=0; i<nFZones; i++)
 		{
-			f_node_loop(f,tFace,n)              /* Loop over all nodes in face Thread tFace */
+			tFace=Lookup_Thread(domain,zoneID[i]);  /* thread pointer to Zone ID */
+			begin_f_loop(f,tFace)					/* loop over all faces in thread */
+			if PRINCIPAL_FACE_P(f,tFace)			/* checks if face truly belongs to computing node: important for faces at the partition interface */
 			{
-				v=F_NODE(f,tFace,n);      /* obtain the global face node number*/
-				nodeX=NODE_X(v);          /* returns real x coordinate of node v to node X*/
-				nodeY=NODE_Y(v);
-				nodeZ=NODE_Z(v);
-				if (N_UDMI(v,myid)==-1)
+				f_node_loop(f,tFace,n)      		/* loop over all nodes in face thread tFace */
 				{
-					fprintf(ptFile,"%d %lg %lg %lg \n", ptID, 1000*nodeX, 1000*nodeY, 1000*nodeZ);
-					N_UDMI(v,myid)=ptID;
-					ptID+=1;
-					ptNum[myid] = ptID;
+					v=F_NODE(f,tFace,n);   			/* obtain the global face node number*/
+					N_UDMI(v,myid)=-1;          	/* access or store the value of the user-defined memory in a mesh node with index of the current core as -1*/
 				}
 			}
+			end_f_loop(f,tFace)
 		}
-		end_f_loop(f,tFace)
-	}
-
-	Message(" \n %d : %d ", myid, ptNum [myid]);
 	
+		/* loop over all face zones. loop over all nodes, that have not been checked (all with entries -1)
+		and connects the node on the core with the index of the point. In other words, point ID and coordinates of each node is assigned in the file "surface" 	*/	
+		for (i=0; i<nFZones; i++)
+		{
+			tFace=Lookup_Thread(domain,zoneID[i]);	/* thread pointer to Zone ID */
+		
+			begin_f_loop(f,tFace)      				/* loop over all faces in a given face thread tFace */
+			if PRINCIPAL_FACE_P(f,tFace)			/* checks if face truly belongs to computing node: important for faces at the partition interface */
+			{
+				f_node_loop(f,tFace,n)              /* loop over all nodes in face Thread tFace */
+				{
+					v=F_NODE(f,tFace,n);      		/* obtain the global face node number */
+					nodeX=NODE_X(v);          		/* returns real x coordinate of node v to nodeX */
+					nodeY=NODE_Y(v);
+					nodeZ=NODE_Z(v);
+					if (N_UDMI(v,myid)==-1)			/* if N_UDMI contains -1, current node belongs to computing node and local node numbering and coordinates will be written to file */
+					{
+						fprintf(ptFile,"%d %lg %lg %lg \n", ptID, 1000*nodeX, 1000*nodeY, 1000*nodeZ);
+						N_UDMI(v,myid)=ptID;		/* local node numbering will be stored in user-defined memory */
+						ptID+=1;
+						ptNum[myid] = ptID;
+					}
+				}
+			}
+			end_f_loop(f,tFace)
+		}
 
-	#if RP_NODE
-  		if (! I_AM_NODE_LAST_P) PRF_CSEND_INT(myid + 1, &dummy, 1, myid);
+		Message(" \n %d : %d ", myid, ptNum[myid]);
+	
+  		if (!I_AM_NODE_LAST_P) PRF_CSEND_INT(myid + 1, &dummy, 1, myid); /* each computing node (except for the last) sends dummy variable to neighbor to the right */
 		fclose(ptFile);
 
-	#else
-		fclose(ptFile);
-	#endif  
-
-#endif  
+	#endif  /* RP_NODE */ 
 }
